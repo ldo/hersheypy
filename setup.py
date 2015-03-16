ec5b1f12aad3d9adc6fc92abf7b0b0705138e1a9
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2015-03-16 22:00:20 +0000

    add setup.py if you wish to use it

---
+++ setup.py
@@ -0,0 +1,22 @@
+#+
+# Distutils script to install the hershey_font.py module. Invoke from
+# the command line in this directory as follows:
+#
+#     python3 setup.py build
+#     sudo python3 setup.py install
+#
+# Written by Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
+#-
+
+import distutils.core
+
+distutils.core.setup \
+  (
+    name = "HersheyPy",
+    version = "0.5",
+    description = "Hershey fonts as user fonts with the Cairo graphics library",
+    author = "Lawrence D'Oliveiro",
+    author_email = "ldo@geek-central.gen.nz",
+    url = "http://github.com/ldo/hersheypy",
+    py_modules = ["hershey_font"],
+  )
